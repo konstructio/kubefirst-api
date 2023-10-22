8aabce092e57e1e8381c07ef77c8bc35cad6352c
Jared Edwards <jared@kubefirst.com>
2023-10-21 21:05:12 -0600

    move client creation

--- main.go
+++ main.go
@@ -10,13 +10,9 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/denisbrodbeck/machineid"
 	"github.com/kubefirst/kubefirst-api/internal/environments"
 	"github.com/kubefirst/kubefirst-api/internal/services"
-	"github.com/kubefirst/kubefirst-api/pkg/metrics"
 	"github.com/kubefirst/kubefirst-api/pkg/segment"
-	"github.com/kubefirst/metrics-client/pkg/telemetry"
-	"github.com/segmentio/analytics-go"
 
 	"github.com/joho/godotenv"
 	"github.com/kubefirst/kubefirst-api/docs"
@@ -37,8 +33,7 @@ import (
 // @BasePath /api/v1
 
 const (
-	port            int    = 8081
-	kubefirstClient string = "api"
+	port int = 8081
 )
 
 func main() {
@@ -117,26 +112,8 @@ func main() {
 	docs.SwaggerInfo.Schemes = []string{"http"}
 
 	// Telemetry handler
-	machineID, _ := machineid.ID()
-	segClient := telemetry.SegmentClient{
-		TelemetryEvent: telemetry.TelemetryEvent{
-			CliVersion:        os.Getenv("KUBEFIRST_VERSION"),
-			CloudProvider:     os.Getenv("CLOUD_PROVIDER"),
-			ClusterID:         os.Getenv("CLUSTER_ID"),
-			ClusterType:       os.Getenv("CLUSTER_TYPE"),
-			DomainName:        os.Getenv("DOMAIN_NAME"),
-			GitProvider:       os.Getenv("GIT_PROVIDER"),
-			InstallMethod:     os.Getenv("INSTALL_METHOD"),
-			KubefirstClient:   kubefirstClient,
-			KubefirstTeam:     os.Getenv("KUBEFIRST_TEAM"),
-			KubefirstTeamInfo: os.Getenv("KUBEFIRST_TEAM_INFO"),
-			MachineID:         machineID,
-			ErrorMessage:      "",
-			UserId:            machineID,
-			MetricName:        metrics.KubefirstHeartbeat,
-		},
-		Client: analytics.New(segment.SegmentIOWriteKey),
-	}
+
+	segClient := segment.InitClient()
 	defer segClient.Client.Close()
 
 	// Subroutine to automatically update gitops catalog
@@ -145,7 +122,7 @@ func main() {
 	// Subroutine to emit heartbeat
 	if useTelemetry {
 
-		go apitelemetry.Heartbeat(&segClient, db.Client)
+		go apitelemetry.Heartbeat(segClient, db.Client)
 	}
 
 	// API
--- pkg/segment/segment.go
+++ pkg/segment/segment.go
@@ -1,8 +1,47 @@
 package segment
 
+import (
+	"os"
+
+	"github.com/denisbrodbeck/machineid"
+	"github.com/kubefirst/kubefirst-api/pkg/metrics"
+	"github.com/kubefirst/runtime/pkg/segment"
+
+	"github.com/kubefirst/metrics-client/pkg/telemetry"
+	"github.com/segmentio/analytics-go"
+)
+
 const (
 	// SegmentIO constants
 	// SegmentIOWriteKey The write key is the unique identifier for a source that tells Segment which source data comes
 	// from, to which workspace the data belongs, and which destinations should receive the data.
-	SegmentIOWriteKey = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
+	SegmentIOWriteKey        = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
+	kubefirstClient   string = "api"
 )
+
+func InitClient() *telemetry.SegmentClient {
+
+	machineID, _ := machineid.ID()
+
+	c := telemetry.SegmentClient{
+		TelemetryEvent: telemetry.TelemetryEvent{
+			CliVersion:        os.Getenv("KUBEFIRST_VERSION"),
+			CloudProvider:     os.Getenv("CLOUD_PROVIDER"),
+			ClusterID:         os.Getenv("CLUSTER_ID"),
+			ClusterType:       os.Getenv("CLUSTER_TYPE"),
+			DomainName:        os.Getenv("DOMAIN_NAME"),
+			GitProvider:       os.Getenv("GIT_PROVIDER"),
+			InstallMethod:     os.Getenv("INSTALL_METHOD"),
+			KubefirstClient:   kubefirstClient,
+			KubefirstTeam:     os.Getenv("KUBEFIRST_TEAM"),
+			KubefirstTeamInfo: os.Getenv("KUBEFIRST_TEAM_INFO"),
+			MachineID:         machineID,
+			ErrorMessage:      "",
+			UserId:            machineID,
+			MetricName:        metrics.KubefirstHeartbeat,
+		},
+		Client: analytics.New(segment.SegmentIOWriteKey),
+	}
+
+	return &c
+}
