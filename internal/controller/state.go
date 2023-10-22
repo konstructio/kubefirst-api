1794dc2e37fb6779d742a6b9fb0119e320c404d5
John Dietz <john@kubefirst.com>
2023-10-21 23:18:20 -0400

    Update internal/controller/state.go
    Co-authored-by: Jared Edwards <jared@kubefirst.com>

--- internal/controller/state.go
+++ internal/controller/state.go
@@ -127,7 +127,7 @@ func (clctrl *ClusterController) StateStoreCredentials() error {
 			if err != nil {
 				msg := fmt.Sprintf("error creating google bucket %s: %s", clctrl.KubefirstStateStoreBucketName, err)
 				log.Error()
-				telemetry.SendCountMetric(clctrl.Telemetry, metrics.StateStoreCreateFailed, msg)
+				telemetry.SendCountMetric(clctrl.Telemetry, metrics.StateStoreCredentialsCreateFailed, msg)
 				return fmt.Errorf(msg)
 			}
 
