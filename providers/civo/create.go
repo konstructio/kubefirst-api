539541eb697acb594ddddcce1aab1dac228e9cff
Jared Edwards <jared@kubefirst.com>
2023-12-08 09:37:32 -0700

    fix missed civo timeout, rm segment file

--- providers/civo/create.go
+++ providers/civo/create.go
@@ -196,7 +196,7 @@ func CreateCivoCluster(definition *pkgtypes.ClusterDefinition) error {
 		"app.kubernetes.io/instance",
 		"crossplane",
 		"crossplane-system",
-		1200,
+		3600,
 	)
 	if err != nil {
 		log.Errorf("Error finding crossplane Deployment: %s", err)
@@ -249,7 +249,7 @@ func CreateCivoCluster(definition *pkgtypes.ClusterDefinition) error {
 		"app.kubernetes.io/name",
 		"kubefirst-api",
 		"kubefirst",
-		3600,
+		1200,
 	)
 	if err != nil {
 		log.Errorf("Error finding kubefirst api Deployment: %s", err)
