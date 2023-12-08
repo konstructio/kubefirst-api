6e12d77f0d82b640d930fc341987ba53b14a29ad
Jared Edwards <jared@kubefirst.com>
2023-12-08 14:15:48 -0700

    make it 4 nodes

--- internal/constants/global.go
+++ internal/constants/global.go
@@ -32,7 +32,7 @@ var cloudProviderDefaults = types.CloudProviderDefaults{
 	Civo:         types.CloudDefault{InstanceSize: "g4s.kube.large", NodeCount: "6"},
 	DigitalOcean: types.CloudDefault{InstanceSize: "s-4vcpu-8gb", NodeCount: "4"},
 	Google:       types.CloudDefault{InstanceSize: "e2-medium", NodeCount: "3"},
-	Vultr:        types.CloudDefault{InstanceSize: "vc2-4c-8gb", NodeCount: "5"},
+	Vultr:        types.CloudDefault{InstanceSize: "vc2-4c-8gb", NodeCount: "4"},
 	K3d:          types.CloudDefault{InstanceSize: "", NodeCount: "3"},
 }
 
