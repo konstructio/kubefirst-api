4e9f9cf00a9d46d0bcf9847df4dadca1517b2789
Derrick <76881293+D-B-Hawk@users.noreply.github.com>
2023-12-08 16:30:20 -0800

    move cloud defaults constant to pkg level for use as source of truth (#264)

--- internal/constants/global.go
+++ internal/constants/global.go
@@ -6,10 +6,6 @@ See the LICENSE file for more details.
 */
 package constants
 
-import (
-	"github.com/kubefirst/kubefirst-api/internal/types"
-)
-
 const (
 	// The Namespace in which Kubefirst runs in-cluster
 	KubefirstNamespace = "kubefirst"
@@ -26,16 +22,3 @@ const (
 
 	SilenceGetEnv = true
 )
-
-var cloudProviderDefaults = types.CloudProviderDefaults{
-	Aws:          types.CloudDefault{InstanceSize: "m5.large", NodeCount: "6"},
-	Civo:         types.CloudDefault{InstanceSize: "g4s.kube.large", NodeCount: "6"},
-	DigitalOcean: types.CloudDefault{InstanceSize: "s-4vcpu-8gb", NodeCount: "4"},
-	Google:       types.CloudDefault{InstanceSize: "e2-medium", NodeCount: "2"},
-	Vultr:        types.CloudDefault{InstanceSize: "vc2-4c-8gb", NodeCount: "4"},
-	K3d:          types.CloudDefault{InstanceSize: "", NodeCount: "3"},
-}
-
-func GetCloudDefaults() types.CloudProviderDefaults {
-	return cloudProviderDefaults
-}
