8d1d5ba2648fd9d1a9e8f9a30186bcefbad9384a
Jared Edwards <jared@kubefirst.com>
2024-03-04 07:02:16 -0700

    initial aws service account code

--- internal/router/api/v1/secrets.go
+++ internal/router/api/v1/secrets.go
@@ -12,10 +12,13 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kubefirst/kubefirst-api/extensions/aws"
 	"github.com/kubefirst/kubefirst-api/internal/constants"
 	"github.com/kubefirst/kubefirst-api/internal/env"
 	"github.com/kubefirst/kubefirst-api/internal/types"
+	"github.com/kubefirst/kubefirst-api/pkg/aws"
 	"github.com/kubefirst/kubefirst-api/pkg/k8s"
+	awsinternal "github.com/kubefirst/runtime/pkg/aws"
 )
 
 func GetClusterSecret(c *gin.Context) {
@@ -35,24 +38,44 @@ func GetClusterSecret(c *gin.Context) {
 		return
 	}
 
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		c.JSON(http.StatusBadRequest, types.JSONFailureResponse{
-			Message: "error finding home directory",
-		})
-		return
+	// todo, need to intelligently figure out how to get the kubeconfig
+	// for the cluster, based on the cloud provider
+	//! init container that uses the environment variable to get the kubeconfig. small image all cloud provider clis, env auth with tokens.
+	var awsConf *awsinternal.AWSConfiguration
+	var kcfg *k8s.KubernetesClient
+
+	//! restConfig in cluster
+
+
+	if os.Getenv("CLOUD_PROVIDER") == "aws" && os.Getenv("IS_CLUSTER_ZERO") == "false" {
+		awsConf = &awsinternal.AWSConfiguration{
+			Config: aws.NewEKSServiceAccountClientV1(),
+		}
+
+		kcfg = aws.CreateEKSKubeconfig(awsConf, clusterName)
+	} else {
+		//! begin
+
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, types.JSONFailureResponse{
+				Message: "error finding home directory",
+			})
+			return
+		}
+
+		clusterDir := fmt.Sprintf("%s/.k1/%s", homeDir, clusterName)
+
+		env, _ := env.GetEnv(constants.SilenceGetEnv)
+
+		var inCluster bool = false
+		if env.InCluster == "true" {
+			inCluster = true
+		}
+
+		kcfg = k8s.CreateKubeConfig(inCluster, fmt.Sprintf("%s/kubeconfig", clusterDir))
+
 	}
-
-	clusterDir := fmt.Sprintf("%s/.k1/%s", homeDir, clusterName)
-
-	env, _ := env.GetEnv(constants.SilenceGetEnv)
-
-	var inCluster bool = false
-	if env.InCluster == "true" {
-		inCluster = true
-	}
-
-	kcfg := k8s.CreateKubeConfig(inCluster, fmt.Sprintf("%s/kubeconfig", clusterDir))
 
 	secrets, err := k8s.ReadSecretV2(kcfg.Clientset, "kubefirst", secret)
 	if err != nil {
