b14b84cce5091437dedeedf004b693ec59a6e271
mrrishi <mrrishi373@gmail.com>
2024-10-15 03:39:50 +0530

    feat:expose clone function for arn

--- pkg/gitClient/gitClient.go
+++ pkg/gitClient/gitClient.go
@@ -5,4 +5,5 @@ import "github.com/konstructio/kubefirst-api/internal/gitClient"
 var (
 	Commit           = gitClient.Commit
 	ClonePrivateRepo = gitClient.ClonePrivateRepo
+	Clone            = gitClient.Clone
 )
